0587f3b1098f8d15f134f95d4e75ee8f6289a8e1
Shane Gray <shane.joseph.gray@gmail.com>
2015-03-19 14:23:07 +0000

    adding in aspnet web api

---
+++ source/Fullstack.Web/Controllers/InvoiceController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Http;
+
+namespace Fullstack.Web.Controllers
+{
+    public class InvoiceController : ApiController
+    {
+        public IHttpActionResult Get() {
+            return this.Ok(new { message = "Hello World" });
+        }
+    }
+}
